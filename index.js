dfc06641a0a2860fc788df192af50038617d165b
Rimuru-Tempest-26 <pkh2662003@gmail.com>
2023-03-27 12:21:50 +0630

    add date

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 import { showBookList, addBook } from './modules/book.js';
 
 const bookForm = document.querySelector('#bookForm');
@@ -58,3 +59,22 @@ const show = (element, link) => {
     }
   });
 });
+
+// add date
+let suffix;
+const date = DateTime.now().toFormat('d');
+
+if (+date[1] === 1 && +date !== 11) {
+  suffix = 'st';
+} else if (+date[1] === 2 && +date !== 12) {
+  suffix = 'nd';
+} else if (+date[1] === 3 && +date !== 13) {
+  suffix = 'rd';
+} else {
+  suffix = 'th';
+}
+
+setInterval(() => {
+  const now = DateTime.now().toFormat(`MMMM dd'${suffix}' yyyy', ' hh:mm:ss a`);
+  document.getElementById('date').innerHTML = now;
+}, 1000);
